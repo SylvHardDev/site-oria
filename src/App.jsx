bc3e9e273bd9c0902df9645c0b47a1fb8f995273
Hardy <hardsyldev@gmail.com>
2024-10-18 13:59:02 +0300

    setup shadcn ui, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+    <div className="py-20">
+      <Button>Hello shadcn</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
