a7d90020227f04d7a6f3aba4e3e888d495ec30e9
Hardy <hardsyldev@gmail.com>
2024-10-22 10:21:58 +0300

    footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,13 +2,14 @@ import "./Footer.css"
 import lamp from "../../assets/icons/lamp.png"
 import phone from "../../assets/icons/phone.png"
 import maps from "../../assets/icons/maps.png"
+import chevron from "../../assets/icons/chevron.png"
 
 export default function Footer() {
   return (
     <footer className="bg-[#29276c] text-white py-12 pb-0">
       <div className="container mx-auto px-24 ">
         {/* Top section with three blocks */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12 ">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
           {/* Block 1: Qui sommes-nous */}
           <div className="flex pl-4 p-2 items-center border border-slate-600 relative">
             <div className="mr-4">
@@ -66,54 +67,54 @@ export default function Footer() {
         </div>
 
         {/* Bottom section */}
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 border-t border-slate-800 pt-10">
           {/* Nos horaires */}
           <div>
-            <h4 className="text-lg font-semibold">Nos horaires</h4>
-            <p className="text-sm">Jours Ouvrables :</p>
-            <p className="text-sm text-gray-400">Lundi - Vendredi : heure</p>
-            <p className="text-sm text-gray-400">Samedi : heure</p>
-            <p className="text-sm">Période de vacances :</p>
-            <p className="text-sm text-gray-400">Toujours ouverts</p>
+            <h4 className="text-sm font-semibold">Nos horaires</h4>
+            <p className="text-xs pt-4 ">Jours Ouvrables :</p>
+            <p className="text-xs pl-4 text-gray-400">Lundi - Vendredi : heure</p>
+            <p className="text-xs pl-4 text-gray-400">Samedi : heure</p>
+            <p className="text-xs pt-4">Période de vacances :</p>
+            <p className="text-xs pl-4 text-gray-400">Toujours ouverts</p>
           </div>
 
           {/* Menu */}
           <div>
-            <h4 className="text-lg font-semibold">Menu</h4>
-            <ul className="text-sm space-y-1">
-              <li><a href="/" className="text-green-500 hover:text-white">ACCUEIL</a></li>
-              <li><a href="/about" className="text-green-500 hover:text-white">A PROPOS</a></li>
-              <li><a href="/filiales" className="text-green-500 hover:text-white">NOS FILIALES</a></li>
-              <li><a href="/projects" className="text-green-500 hover:text-white">NOS PROJETS</a></li>
-              <li><a href="/contact" className="text-green-500 hover:text-white">CONTACTS</a></li>
+            <h4 className="text-sm font-semibold">Menu</h4>
+            <ul className="pt-3 text-xs space-y-1">
+              <li className="border-b border-slate-800 flex items-center"><img className="w-3 h-2" src={chevron} alt="icon chevron" /> <a href="/" className="p-2 block text-slate-400 hover:text-white">ACCUEIL</a></li>
+              <li className="border-b border-slate-800 flex items-center"><img className="w-3 h-2" src={chevron} alt="icon chevron" /><a href="/about" className="p-2 block text-slate-400 hover:text-white">A PROPOS</a></li>
+              <li className="border-b border-slate-800 flex items-center"><img className="w-3 h-2" src={chevron} alt="icon chevron" /><a href="/filiales" className="p-2 block text-slate-400 hover:text-white">NOS FILIALES</a></li>
+              <li className="border-b border-slate-800 flex items-center"><img className="w-3 h-2" src={chevron} alt="icon chevron" /><a href="/projects" className="p-2 block text-slate-400 hover:text-white">NOS PROJETS</a></li>
+              <li className="flex items-center"><img className="w-3 h-2" src={chevron} alt="icon chevron" /><a href="/contact" className="p-2 block text-slate-400 hover:text-white">CONTACTS</a></li>
             </ul>
           </div>
 
           {/* Commentaires */}
           <div>
-            <h4 className="text-lg font-semibold">Commentaires</h4>
-            <p className="text-sm">
+            <h4 className="text-sm font-semibold">Commentaires</h4>
+            <p className="text-xs pt-2 text-slate-400">
               <span className="text-green-500">@Pseudo</span> Je recommande vivement le Groupe ORIA pour tous vos projets immobiliers. Leurs réalisations sont synonymes de qualité et de confort.
             </p>
-            <p className="text-xs text-gray-400">Il y a 2 heures</p>
+            <p className="text-xs text-slate-200 pt-2">Il y a 2 heures</p>
 
-            <p className="text-sm mt-4">
+            <p className="text-xs text-slate-400 mt-4 pt-2">
               <span className="text-green-500">@Pseudo</span> J ai été impressionné par la qualité des constructions du Groupe ORIA ! Leurs projets sont esthétiques et fonctionnels. Un vrai savoir-faire local.
             </p>
-            <p className="text-xs text-gray-400">Il y a 2 heures</p>
+            <p className="text-xs text-slate-200 pt-2">Il y a 2 heures</p>
           </div>
 
           {/* Nos actus */}
           <div>
-            <h4 className="text-lg font-semibold">Nos actus</h4>
-            <ul className="text-sm space-y-4">
-              <li>
-                <a href="#" className="hover:text-white">PIONNIER DE LA CONSTRUCTION À M/CAR</a>
-                <p className="text-xs text-gray-400">Le: 25 Septembre 2024</p>
+            <h4 className="text-sm font-semibold">Nos actus</h4>
+            <ul className="text-xs space-y-4 pt-2">
+              <li className="border-b border-slate-800 pb-4">
+                <a href="#" className="hover:text-white text-slate-400 pt-2">PIONNIER DE LA CONSTRUCTION À M/CAR</a>
+                <p className="text-xs text-gray-200 pt-2">Le: 25 Septembre 2024</p>
               </li>
               <li>
-                <a href="#" className="hover:text-white">PIONNIER DE LA CONSTRUCTION À M/CAR</a>
-                <p className="text-xs text-gray-400">Le: 25 Septembre 2024</p>
+                <a href="#" className="hover:text-white text-slate-400">PIONNIER DE LA CONSTRUCTION À M/CAR</a>
+                <p className="text-xs text-gray-200 pt-2">Le: 25 Septembre 2024</p>
               </li>
             </ul>
           </div>
