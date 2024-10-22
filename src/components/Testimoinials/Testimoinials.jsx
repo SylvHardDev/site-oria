5387f45778f0fce9022b636b7537d4408126f556
Mihajatiana <miandriakotonerason@gmail.com>
2024-10-22 11:19:57 +0300

    add: finish testimoinials components

--- src/components/Testimoinials/Testimoinials.jsx
+++ src/components/Testimoinials/Testimoinials.jsx
@@ -13,79 +13,84 @@ import { Button } from "../ui/button"
 
 function Testimoinials() {
   return (
-    <div id="testimoinials" className="h-auto">
-{/* <div className="layer h-80"></div> */}
-      <h2 className="text-green-500 text-sm"><span className="text-white">■</span> CE QUE NOS CLIENTS PENSENT DE NOS SERVICES</h2>
-      <h3 className="text-white">TEMOIGNAGES</h3>
-      <div className="flex ">
-        <p>Les témoignages de nos clients témoignent de notre engagement à offrir des services immobiliers de qualités supérieure. Leurs retours positifs confirment notre expertise et notre capacité à répondre à leurs besoins spécifiques.</p>
-        <Button>PLUS</Button>
-      </div>
-<Carousel>
-  <CarouselContent>
-    <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
-      <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
-        <img src={crochet} className="w-20" />
-        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
-      </div>
-      <div className="w-80 flex">
-        <img src={person1} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
-        <div>
-          <h4 className="text-white">Nom de la personne</h4>
-          <p className="text-green-500 text-xs">Poste, Nom de la société</p>
+    <section id="testimoinials" className="bg-white py-20 relative">
+      <div className="layer z-[-4]"></div>
+
+      <div className="container mx-auto px-32">
+        <h2 className="text-green-500 text-xs"><span className="text-white">■</span> CE QUE NOS CLIENTS PENSENT DE NOS SERVICES</h2>
+        <h3 className="text-white text-3xl">TEMOIGNAGES</h3>
+        <div className="mt-2 text-[15px] text-gray-400 md:flex w-full items-center justify-between">
+          <p className="w-3/4">Les témoignages de nos clients témoignent de notre engagement à offrir des services immobiliers de qualités supérieure. Leurs retours positifs confirment notre expertise et notre capacité à répondre à leurs besoins spécifiques.</p>
+          <Button className="inline-block backdrop-grayscale-0 w-40 bg-green-600">PLUS</Button>
         </div>
-      </div>
-    </CarouselItem>
 
-   <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
-      <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
-        <img src={crochet} className="w-20" />
-        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
-      </div>
-      <div className="w-80 flex">
-        <img src={person2} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
-        <div>
-          <h4 className="text-white">Nom de la personne</h4>
-          <p className="text-green-500 text-xs">Poste, Nom de la société</p>
-        </div>
-      </div>
-    </CarouselItem>
+      {/* DEBUT CAROUSEL */}
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
+            <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
+              <img src={crochet} className="w-20" />
+              <p className="text-sm text-gray-400 text-">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
+            </div>
+            <div className="w-80 flex">
+              <img src={person1} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
+              <div>
+                <h4 className="text-white">Nom de la personne</h4>
+                <p className="text-green-500 text-xs">Poste, Nom de la société</p>
+              </div>
+            </div>
+          </CarouselItem>
 
-
-<CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
-      <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
-        <img src={crochet} className="w-20" />
-        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
-      </div>
-      <div className="w-80 flex">
-        <img src={person3} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
-        <div>
-          <h4 className="text-white">Nom de la personne</h4>
-          <p className="text-green-500 text-xs">Poste, Nom de la société</p>
-        </div>
-      </div>
-    </CarouselItem>
-
-  <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
-        <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
-          <img src={crochet} className="w-20" />
-          <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
-        </div>
-        <div className="w-80 flex">
-          <img src={person4} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
-          <div>
-            <h4 className="text-white">Nom de la personne</h4>
-            <p className="text-green-500 text-xs">Poste, Nom de la société</p>
-          </div>
-        </div>
-      </CarouselItem>
+        <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
+            <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
+              <img src={crochet} className="w-20" />
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
+            </div>
+            <div className="w-80 flex">
+              <img src={person2} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
+              <div>
+                <h4 className="text-white">Nom de la personne</h4>
+                <p className="text-green-500 text-xs">Poste, Nom de la société</p>
+              </div>
+            </div>
+          </CarouselItem>
 
 
+      <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
+            <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
+              <img src={crochet} className="w-20" />
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
+            </div>
+            <div className="w-80 flex">
+              <img src={person3} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
+              <div>
+                <h4 className="text-white">Nom de la personne</h4>
+                <p className="text-green-500 text-xs">Poste, Nom de la société</p>
+              </div>
+            </div>
+          </CarouselItem>
 
-  </CarouselContent>
-</Carousel>
+        <CarouselItem className="md:basis-1/2 lg:basis-1/3 h-96 flex flex-col items-center justify-center">
+              <div id="clip-testimoinial" className="bg-white w-80 h-40 flex items-start justify-evenly p-4">
+                <img src={crochet} className="w-20" />
+                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam obcaecati ipsum eum doloribus nisi aut ab mollitia consectetur.</p>
+              </div>
+              <div className="w-80 flex">
+                <img src={person4} alt="afro-american-woman-smiling-close-up" className="w-16 mr-3" />
+                <div>
+                  <h4 className="text-white">Nom de la personne</h4>
+                  <p className="text-green-500 text-xs">Poste, Nom de la société</p>
+                </div>
+              </div>
+            </CarouselItem>
+
+
+
+        </CarouselContent>
+      </Carousel>
 
     </div>
+    </section>
   )
 }
 
