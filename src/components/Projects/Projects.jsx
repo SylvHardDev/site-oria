813ab176471c9083d763733f67aad62eed753d0d
Mihajatiana <miandriakotonerason@gmail.com>
2024-10-23 18:36:03 +0300

    add: Projects components

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+function Projects() {
+  return (
+    <section className="bg-white py-20 ">
+      <div className="container mx-auto px-32">
+        <h1>hello projects</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
