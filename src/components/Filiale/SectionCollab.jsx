194a18790926ba4213f03dee3182b8f86ddba5ff
Hardy <hardsyldev@gmail.com>
2024-10-23 15:53:52 +0300

    fix: border section collaboration

--- src/components/Filiale/SectionCollab.jsx
+++ src/components/Filiale/SectionCollab.jsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button"
 function SectionCollab() {
   return (
     <div className="section-collab w-full h-[30vh] px-32 flex items-center justify-center bg-green-700">
-      <div className="p-4 rounded-sm flex items-center justify-between border">
+      <div className="p-4 rounded-sm flex items-center justify-between border border-[#ffffff42]">
         <p className="mt-2 text-[15px] w-2/3 text-white">
           Confiez vos projets immobiliers à HCM : notre expertise dans le
           bâtiment, le génie civil et les ouvrages d art, associée à notre
