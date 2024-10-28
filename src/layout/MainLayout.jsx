61d8cd8cc878313d4a65ecd5818dd33157ed61a3
Hardy <hardsyldev@gmail.com>
2024-10-28 11:41:11 +0300

    remove component testimonial if contact page

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,15 +2,18 @@ import Navbar from "@/components/Navbar/Navbar"
 import Footer from "@/components/Footer/Footer"
 import Clients from "@/components/NosCliensts/Clients"
 import Testimoinials from "@/components/Testimoinials/Testimoinials"
+import { useLocation } from "react-router-dom";
 
 function MainLayout({children}) {
+  const location = useLocation();
+const isContactPage = location.pathname === "/contact";
   return (
     <>
       <Navbar/>
         <main>
           {children}
         </main>
-      <Testimoinials />
+      {!isContactPage && <Testimoinials />}
       <Clients />
       <Footer/>
     </>
