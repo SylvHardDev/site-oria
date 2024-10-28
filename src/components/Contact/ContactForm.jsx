904b59ac1e8a052f7f1d1433692fb542cf04edb1
Mihajatiana <miandriakotonerason@gmail.com>
2024-10-28 15:55:01 +0300

    fix: resp Contact page

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,19 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-import phone from "../../assets/icons/phone-vert.png"
-import maps from "../../assets/icons/location-vert.png"
-import mail from "../../assets/icons/MAIL.png"
+import maps from "../../assets/icons/location-vert.png";
+import mail from "../../assets/icons/MAIL.png";
+import phone from "../../assets/icons/phone-vert.png";
 
 function ContactForm() {
   return (
-    <div className="text-sm items-center justify-center w-full py-10 px-32 ">
-      <div className="container mx-auto p-10 lg:flex lg:space-x-10 lg:items-start">
-
+    <div className="text-sm items-center justify-center w-full py-10 px-4 md:px-32 ">
+      <div className="container mx-auto lg:flex lg:space-x-10 lg:items-start">
         {/* Section Formulaire et informations de contact */}
-        <div className="flex gap-10 w-full mx-auto my-auto">
+        <div className="flex flex-col lg:flex-row lg:gap-10 p-4 md:p-10">
           {/* Formulaire de contact */}
-          <form className="space-y-4 w-1/2">
+          <form className="space-y-4 lg:w-1/2 mb-4">
             <div className="flex space-x-4">
               <Input
                 label="Nom"
@@ -43,32 +42,38 @@ function ContactForm() {
             </Button>
           </form>
 
-          <div className="right w-1/2 flex-col">
-
+          <div className="right md:w-1/2 flex-col">
             {/* Texte d'introduction */}
             <div className="space-y-4 text-gray-700">
               <p>
-                Lorem ipsum dolor sit amet, adipiscing condimentum tristique vel, eleifend sed turpis.
+                Lorem ipsum dolor sit amet, adipiscing condimentum tristique
+                vel, eleifend sed turpis.
               </p>
-              <p>
-                Amet, consectetur adipiscing elit Integer.
-              </p>
+              <p>Amet, consectetur adipiscing elit Integer.</p>
             </div>
 
             {/* Informations de contact */}
             <div className="space-y-4 mt-4 text-gray-700">
-              <div className="flex justify-between">
+              <div className="flex flex-col md:flex-row justify-between">
                 <div className="flex-col space-x-2">
                   <p>Phone :</p>
                   <div className="flex space-x-2">
-                    <img className="w-4 h-4 my-auto" src={phone} alt="icone telephone" />
+                    <img
+                      className="w-4 h-4 my-auto"
+                      src={phone}
+                      alt="icone telephone"
+                    />
                     <span>Phone : 020 23 314 14</span>
                   </div>
                 </div>
                 <div className="flex-col space-x-2">
                   <p>Email :</p>
                   <div className="flex items-center space-x-2">
-                    <img className="w-4 h-4 my-auto" src={mail} alt="icone telephone" />
+                    <img
+                      className="w-4 h-4 my-auto"
+                      src={mail}
+                      alt="icone telephone"
+                    />
                     <span>example@domain.com</span>
                   </div>
                 </div>
@@ -85,7 +90,7 @@ function ContactForm() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
--- src/components/Contact/ContactHero.jsx
+++ src/components/Contact/ContactHero.jsx
@@ -1,14 +1,14 @@
-import "./Contact.css"
+import "./Contact.css";
 
 function ContactHero() {
   return (
     <section className="contact-hero w-full h-[30vh] flex justify-center items-center flex-col relative">
       <div className="contact-layer"></div>
       <h3 className="text-green-500 text-xs">NOUS SOMME LA POUR VOUS!</h3>
-      <h1 className="text-5xl text-white">CONTACTEZ-NOUS</h1>
+      <h1 className="text-3xl md:text-5xl text-white">CONTACTEZ-NOUS</h1>
       <h3 className="text-xs mt-3 text-white">Accueil / Contacts</h3>
     </section>
-  )
+  );
 }
 
-export default ContactHero
+export default ContactHero;
