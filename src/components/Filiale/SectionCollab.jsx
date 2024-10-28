f416f6528440be91b5c4520cb02c3bda894265b6
Mihajatiana <miandriakotonerason@gmail.com>
2024-10-28 15:42:08 +0300

    fix: resp Filiale page

--- src/components/Filiale/SectionCollab.jsx
+++ src/components/Filiale/SectionCollab.jsx
@@ -2,7 +2,7 @@ import { Button } from "../ui/button";
 
 function SectionCollab() {
   return (
-    <div className="section-collab w-full h-[30vh] px-4 md:px-32 flex items-center justify-center bg-green-700">
+    <div className="section-collab w-full h-[40vh] md:h-[30vh] px-4 md:px-32 flex items-center justify-center bg-green-700">
       <div className="p-4 rounded-sm flex flex-col md:flex-row md:items-center md:justify-between border border-[#ffffff42]">
         <p className="my-2 text-[15px] md:w-2/3 text-white">
           Confiez vos projets immobiliers à HCM : notre expertise dans le
--- src/components/Filiale/SectionSCI.jsx
+++ src/components/Filiale/SectionSCI.jsx
@@ -1,16 +1,15 @@
+import classeur from "../../assets/icons/classeur-gray.png";
+import construct from "../../assets/icons/double-house-gray.png";
+import goute from "../../assets/icons/goute-gray.png";
+import house from "../../assets/icons/house-gray.png";
+import setting from "../../assets/icons/setting.png";
+import logo_SCI from "../../assets/logo/Logo_SCI_result.webp";
 import { Button } from "../ui/button";
-import construct from "../../assets/icons/double-house-gray.png"
-import goute from "../../assets/icons/goute-gray.png"
-import house from "../../assets/icons/house-gray.png"
-import classeur from "../../assets/icons/classeur-gray.png"
-import setting from "../../assets/icons/setting.png"
-import logo_SCI from "../../assets/logo/Logo_SCI_result.webp"
-
 
 function SectionSCI() {
   return (
     <section className="py-12">
-      <div className="container mx-auto px-32">
+      <div className="container mx-auto px-4 md:px-32">
         {/* Titre principal */}
         <div>
           <h2 className="text-blue-600 text-xs uppercase mb-2">
@@ -20,14 +19,14 @@ function SectionSCI() {
           <h1 className="text-2xl font-bold mb-4">SCI-DM</h1>
           <p className="text-sm text-gray-600 mb-8">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-            illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-            explicabo.
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
           </p>
         </div>
 
         {/* Grille de services */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid  md:grid-cols-2 lg:grid-cols3 gap-6">
           {/* Carrelage */}
           <div className="bg-blue-900 text-white p-6 rounded-lg">
             <div className="title  flex justify-between">
@@ -41,8 +40,8 @@ function SectionSCI() {
             </div>
             <p className="text-sm mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-              volutpat.
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
             </p>
             <Button className="bg-green-600 text-white py-2 px-4 rounded">
               Lire plus
@@ -51,7 +50,7 @@ function SectionSCI() {
 
           {/* Rénovations */}
           <div className="border p-6 rounded-lg">
-          <div className="title  flex justify-between">
+            <div className="title  flex justify-between">
               <div className="mb-4 flex flex-col">
                 <span className="inline-block w-2 h-2 bg-black mr-2 "></span>
                 <h3 className="inline-block text-2xl pt-4">Rénovation</h3>
@@ -62,8 +61,8 @@ function SectionSCI() {
             </div>
             <p className="text-sm text-gray-600 mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-              volutpat.
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
             </p>
             <Button className="bg-green-600 text-white py-2 px-4 rounded">
               Lire plus
@@ -72,10 +71,12 @@ function SectionSCI() {
 
           {/* Design et construction */}
           <div className="border p-6 rounded-lg">
-          <div className="title  flex justify-between">
+            <div className="title  flex justify-between">
               <div className="mb-4 flex flex-col">
                 <span className="inline-block w-2 h-2 bg-black mr-2 "></span>
-                <h3 className="inline-block text-2xl pt-4">Design et construction</h3>
+                <h3 className="inline-block text-2xl pt-4">
+                  Design et construction
+                </h3>
               </div>
               <div className="icon p-2 w-12 h-12 bg-slate-100 rounded-sm">
                 <img src={construct} alt="icone goute" />
@@ -83,8 +84,8 @@ function SectionSCI() {
             </div>
             <p className="text-sm text-gray-600 mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-              volutpat.
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
             </p>
             <Button className="bg-green-600 text-white py-2 px-4 rounded">
               Lire plus
@@ -93,7 +94,7 @@ function SectionSCI() {
 
           {/* Consulting */}
           <div className="border p-6 rounded-lg">
-          <div className="title  flex justify-between">
+            <div className="title  flex justify-between">
               <div className="mb-4 flex flex-col">
                 <span className="inline-block w-2 h-2 bg-black mr-2 "></span>
                 <h3 className="inline-block text-2xl pt-4">Consulting</h3>
@@ -104,8 +105,8 @@ function SectionSCI() {
             </div>
             <p className="text-sm text-gray-600 mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-              volutpat.
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
             </p>
             <Button className="bg-green-600 text-white py-2 px-4 rounded">
               Lire plus
@@ -114,7 +115,7 @@ function SectionSCI() {
 
           {/* Management */}
           <div className="border p-6 rounded-lg">
-          <div className="title  flex justify-between">
+            <div className="title  flex justify-between">
               <div className="mb-4 flex flex-col">
                 <span className="inline-block w-2 h-2 bg-black mr-2"></span>
                 <h3 className="inline-block text-2xl pt-4">Management</h3>
@@ -125,8 +126,8 @@ function SectionSCI() {
             </div>
             <p className="text-sm text-gray-600 mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-              volutpat.
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
             </p>
             <Button className="bg-green-600 text-white py-2 px-4 rounded">
               Lire plus
@@ -135,11 +136,7 @@ function SectionSCI() {
 
           {/* Logo */}
           <div className="flex justify-center items-center">
-            <img
-              src={logo_SCI}
-              alt="HCM logo"
-              className="h-52 w-auto"
-            />
+            <img src={logo_SCI} alt="HCM logo" className="h-52 w-auto" />
           </div>
         </div>
       </div>
@@ -147,4 +144,4 @@ function SectionSCI() {
   );
 }
 
-export default SectionSCI
+export default SectionSCI;
