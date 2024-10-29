1521de58f2ed7180901b342d8f746dfc715013c0
Hardy <hardsyldev@gmail.com>
2024-10-29 09:20:06 +0300

    integration section image

---
+++ src/components/Contact/SectionImage.jsx
@@ -0,0 +1,9 @@
+function SectionImage() {
+  return (
+    <div className="px-32 py-20 h-[80vh]">
+      <div className="bg-image h-full w-full"></div>
+    </div>
+  )
+}
+
+export default SectionImage
