3f6e8045cfbc94c381061a26c98a47a5751494f7
Mihajatiana <miandriakotonerason@gmail.com>
2024-10-29 13:55:40 +0300

    fix: hero carousel

--- src/components/Contact/SectionImage.jsx
+++ src/components/Contact/SectionImage.jsx
@@ -1,6 +1,6 @@
 function SectionImage() {
   return (
-    <div className="px-4 md:px-32 py-20 h-[50vh] lg:h-[80vh] ">
+    <div className="px-4 md:px-32 py-11 h-80 lg:h-[80vh] ">
       <div className="bg-image h-full w-full rounded-md"></div>
     </div>
   );
