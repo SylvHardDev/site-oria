d598e1b24e4bda438668b3a8d14db349e096fda9
Mihajatiana <miandriakotonerason@gmail.com>
2024-10-29 10:15:19 +0300

    fix: emrge

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,112 +1,119 @@
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-import phone from "../../assets/icons/phone-vert.png"
-import maps from "../../assets/icons/location-vert.png"
-import mail from "../../assets/icons/MAIL.png"
+// function ContactForm() {
+//   return (
+
+//     <div className="text-sm items-center justify-center w-full py-10 px-32 ">
+//       <div className="container mx-auto pb-10 lg:flex lg:space-x-10 lg:items-start">
+
+//         {/* Section Formulaire et informations de contact */}
+//         <div className="flex gap-10 w-full mx-auto my-auto">
+//           {/* Formulaire de contact */}
+//           <form className="space-y-4 w-1/2">
+//             <div className="flex space-x-4">
+//               <Input
+//                 label="Nom"
+//                 placeholder="Nom"
+//                 className="flex-1"
+//                 required
+//               />
+//               <Input
+//                 label="Email"
+//                 placeholder="Email"
+//                 type="email"
+//                 className="flex-1"
+//                 required
+//               />
+//             </div>
+
+//             <Textarea
+//               label="Message"
+//               placeholder="Votre message"
+//               rows={4}
+//               className="w-full"
+//               required
+//             />
+
+//             <Button type="submit" className="w-full mt-4 bg-green-700">
+//               Envoyer le message
+//             </Button>
+//           </form>
+
+// <<<<<<< HEAD
+//           <div className="right lg:w-1/2 flex-col">
+// =======
+//           <div className="right w-1/2 flex-col">
+
+// >>>>>>> 6618d3f7273a6ee5c056d8f43f98237736b354ae
+//             {/* Texte d'introduction */}
+//             <div className="space-y-4 text-gray-700">
+//               <p>
+//                 Lorem ipsum dolor sit amet, adipiscing condimentum tristique vel, eleifend sed turpis.
+//               </p>
+//               <p>
+//                 Amet, consectetur adipiscing elit Integer.
+//               </p>
+//             </div>
+
+//             {/* Informations de contact */}
+//             <div className="space-y-4 mt-4 text-gray-700">
+// <<<<<<< HEAD
+//               <div className="flex flex-col md:flex-row w-[100%] justify-between">
+//                 <div className="flex-col space-x-2">
+//                   <p>Phone :</p>
+//                   <div className="flex space-x-2">
+//                     <img
+//                       className="w-4 h-4 my-auto"
+//                       src={phone}
+//                       alt="icone telephone"
+//                     />
+//                     <span>020 23 314 14</span>
+// =======
+//               <div className="flex justify-between">
+//                 <div className="flex-col space-x-2">
+//                   <p>Phone :</p>
+//                   <div className="flex space-x-2 space-y-2">
+//                     <img className="w-4 h-4 my-auto" src={phone} alt="icone telephone" />
+//                     <span>Phone : 020 23 314 14</span>
+// >>>>>>> 6618d3f7273a6ee5c056d8f43f98237736b354ae
+//                   </div>
+//                 </div>
+
+//                 <div className="flex-col space-x-2">
+//                   <p>Email :</p>
+// <<<<<<< HEAD
+//                   <div className="flex items-center space-x-2">
+//                     <img
+//                       className="w-4 h-4 "
+//                       src={mail}
+//                       alt="icone telephone"
+//                     />
+//                     <p>example@domain.com</p>
+// =======
+//                   <div className="flex items-center justify-center space-x-2 space-y-2 bg-slate-400">
+//                     <img className="w-4 h-4" src={mail} alt="icone telephone" />
+//                     <span className="inline-block bg-slate-300">example@domain.com</span>
+// >>>>>>> 6618d3f7273a6ee5c056d8f43f98237736b354ae
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="flex-col  ">
+//                 <p>Adresse :</p>
+//                 <div className="flex items-center space-x-2 space-y-2">
+//                   <img className="w-4 h-4" src={maps} alt="icone maps" />
+//                   <span>lorem ipsum dolor sit amet</span>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ContactFormr
 
 function ContactForm() {
-  return (
-    <div className="text-sm items-center justify-center w-full py-10 px-32 ">
-
-      <div className="container mx-auto pb-10 lg:flex lg:space-x-10 lg:items-start">
-
-        {/* Section Formulaire et informations de contact */}
-        <div className="flex gap-10 w-full mx-auto my-auto">
-          {/* Formulaire de contact */}
-          <form className="space-y-4 w-1/2">
-            <div className="flex space-x-4">
-              <Input
-                label="Nom"
-                placeholder="Nom"
-                className="flex-1"
-                required
-              />
-              <Input
-                label="Email"
-                placeholder="Email"
-                type="email"
-                className="flex-1"
-                required
-              />
-            </div>
-
-            <Textarea
-              label="Message"
-              placeholder="Votre message"
-              rows={4}
-              className="w-full"
-              required
-            />
-
-            <Button type="submit" className="w-full mt-4 bg-green-700">
-              Envoyer le message
-            </Button>
-          </form>
-
-          <div className="right lg:w-1/2 flex-col">
-
-            {/* Texte d'introduction */}
-            <div className="space-y-4 text-gray-700">
-              <p>
-                Lorem ipsum dolor sit amet, adipiscing condimentum tristique vel, eleifend sed turpis.
-              </p>
-              <p>
-                Amet, consectetur adipiscing elit Integer.
-              </p>
-            </div>
-
-            {/* Informations de contact */}
-            <div className="space-y-4 mt-4 text-gray-700">
-              <div className="flex flex-col md:flex-row w-[100%] justify-between">
-                <div className="flex-col space-x-2">
-                  <p>Phone :</p>
-                  <div className="flex space-x-2">
-                    <img
-                      className="w-4 h-4 my-auto"
-                      src={phone}
-                      alt="icone telephone"
-                    />
-                    <span>020 23 314 14</span>
-              <div className="flex justify-between">
-                <div className="flex-col space-x-2">
-                  <p>Phone :</p>
-                  <div className="flex space-x-2 space-y-2">
-                    <img className="w-4 h-4 my-auto" src={phone} alt="icone telephone" />
-                    <span>Phone : 020 23 314 14</span>
-                  </div>
-                </div>
-
-                <div className="flex-col space-x-2">
-                  <p>Email :</p>
-                  <div className="flex items-center space-x-2">
-                    <img
-                      className="w-4 h-4 "
-                      src={mail}
-                      alt="icone telephone"
-                    />
-                    <p>example@domain.com</p>
-                  <div className="flex items-center justify-center space-x-2 space-y-2 bg-slate-400">
-                    <img className="w-4 h-4" src={mail} alt="icone telephone" />
-                    <span className="inline-block bg-slate-300">example@domain.com</span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex-col  ">
-                <p>Adresse :</p>
-                <div className="flex items-center space-x-2 space-y-2">
-                  <img className="w-4 h-4" src={maps} alt="icone maps" />
-                  <span>lorem ipsum dolor sit amet</span>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-  )
+  return <div>ContactForm</div>;
 }
 
-export default ContactForm
+export default ContactForm;
