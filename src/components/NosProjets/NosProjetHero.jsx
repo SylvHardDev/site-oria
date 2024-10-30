561958a769a4f99c651b0deec5505d8b5a090a87
Hardy <hardsyldev@gmail.com>
2024-10-30 09:32:52 +0300

    nos projet

--- src/components/NosProjets/NosProjetHero.jsx
+++ src/components/NosProjets/NosProjetHero.jsx
@@ -2,7 +2,12 @@ import "./NosProjet.css"
 
 function NosProjetHero() {
   return (
-    <div>NosProjetHero</div>
+    <section className="nos-projet-hero w-full h-[30vh] flex justify-center items-center flex-col relative">
+      <div className="nos-projet-layer"></div>
+      <h3 className="text-green-500 text-xs">HCM | SCI DM</h3>
+      <h1 className="text-3xl md:text-5xl text-white">NOS PROJETS</h1>
+      <h3 className="text-xs mt-3 text-white">Accueil / Nos projets</h3>
+    </section>
   )
 }
 
