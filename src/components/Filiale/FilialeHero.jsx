4ebba0e5b9b8fd94415a4159ac29168fb0b9c85f
Hardy <hardsyldev@gmail.com>
2024-10-31 09:26:23 +0300

    fix resp

--- src/components/Filiale/FilialeHero.jsx
+++ src/components/Filiale/FilialeHero.jsx
@@ -5,7 +5,7 @@ function FilialeHero() {
     <section className="filiale-hero w-full h-[30vh] flex justify-center items-center flex-col relative">
       <div className="filiale-layer"></div>
       <h3 className="reveal text-green-500 text-xs">HCM | SCI DM</h3>
-      <h1 className="reveal-delay reveal md:text-5xl text-white">NOS FILIALES</h1>
+      <h1 className="reveal-delay reveal text-3xl md:text-5xl text-white">NOS FILIALES</h1>
       <h3 className="reveal reveal-delay text-xs mt-3 text-white">Accueil / Nos filiales</h3>
     </section>
   );
