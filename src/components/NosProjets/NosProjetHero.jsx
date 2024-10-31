69e50f80f3a3da6a962544d4c2c97ea7ceddb42f
Hardy <hardsyldev@gmail.com>
2024-10-31 08:52:09 +0300

    add reveal animation

--- src/components/NosProjets/NosProjetHero.jsx
+++ src/components/NosProjets/NosProjetHero.jsx
@@ -4,9 +4,9 @@ function NosProjetHero() {
   return (
     <section className="nos-projet-hero w-full h-[30vh] flex justify-center items-center flex-col relative">
       <div className="nos-projet-layer"></div>
-      <h3 className="text-green-500 text-xs">HCM | SCI DM</h3>
-      <h1 className="text-3xl md:text-5xl text-white">NOS PROJETS</h1>
-      <h3 className="text-xs mt-3 text-white">Accueil / Nos projets</h3>
+      <h3 className="reveal text-green-500 text-xs">HCM | SCI DM</h3>
+      <h1 className="reveal-delay reveal text-3xl md:text-5xl text-white">NOS PROJETS</h1>
+      <h3 className="reveal-delay reveal text-xs mt-3 text-white">Accueil / Nos projets</h3>
     </section>
   )
 }
