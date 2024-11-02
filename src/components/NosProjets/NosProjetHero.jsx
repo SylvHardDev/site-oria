364f28634f0021b01846243bcdcd6b39af1bab93
Mihajatiana <miandriakotonerason@gmail.com>
2024-11-02 09:18:18 +0300

    fix: hero section

--- src/components/NosProjets/NosProjetHero.jsx
+++ src/components/NosProjets/NosProjetHero.jsx
@@ -1,14 +1,18 @@
-import "./NosProjet.css"
+import "./NosProjet.css";
 
 function NosProjetHero() {
   return (
-    <section className="nos-projet-hero w-full h-[30vh] flex justify-center items-center flex-col relative">
+    <section className="nos-projet-hero w-full mt-16 h-[30vh] flex justify-center items-center flex-col relative">
       <div className="nos-projet-layer"></div>
       <h3 className="reveal text-green-500 text-xs">HCM | SCI DM</h3>
-      <h1 className="reveal-delay reveal text-3xl md:text-5xl text-white">NOS PROJETS</h1>
-      <h3 className="reveal-delay reveal text-xs mt-3 text-white">Accueil / Nos projets</h3>
+      <h1 className="reveal-delay reveal text-3xl md:text-5xl text-white">
+        NOS PROJETS
+      </h1>
+      <h3 className="reveal-delay reveal text-xs mt-3 text-white">
+        Accueil / Nos projets
+      </h3>
     </section>
-  )
+  );
 }
 
-export default NosProjetHero
+export default NosProjetHero;
