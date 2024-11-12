0cfd5c26512ebb074d32cd5afd78a66b088a4205
Hardy <hardsyldev@gmail.com>
2024-11-12 06:47:21 +0300

    clear import

--- src/components/FilialeHCM/FilialeHCM.jsx
+++ src/components/FilialeHCM/FilialeHCM.jsx
@@ -1,8 +1,6 @@
 import batiments from "../../assets/images/BATIMENTS.png";
 import genie from "../../assets/images/GENIE_CIVIL.png";
 import reseau from "../../assets/images/réseaux.png";
-// import logo_htc from "../../assets/logo/logo_HCM.png";
-// import logo_htc from "@/assets/logo/logo_HCM.png"
 const logo_htc = "/assets/logo/logo_HCM.png"
 import { Button } from "../ui/button";
 import "./FilialeHCM.css";
