6654e556ef3905b46cdf715dfe312a063c7aa447
Hardy <hardsyldev@gmail.com>
2024-12-06 14:18:45 +0300

    fix parallax

--- src/components/About/AboutHero.jsx
+++ src/components/About/AboutHero.jsx
@@ -21,7 +21,7 @@ function AboutHero() {
     <section className="about-hero w-full mt-16 h-[30vh] flex justify-center items-center flex-col relative"
       style={{
         background: isImageLoaded
-          ? `url(${bg_hero}) center/cover no-repeat` : "",
+          ? `url(${bg_hero}) center/cover no-repeat background-attachment: fixed background-position: bottom` : "",
       }}>
       {!isImageLoaded && (
         <BlurhashCanvas
