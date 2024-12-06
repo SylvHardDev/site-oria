c9954d963e7778fcf261aeafa5fa387a2cda5e6a
Hardy <hardsyldev@gmail.com>
2024-12-06 11:55:01 +0300

    add lazy loading to the image chantier

--- src/components/Contact/SectionImage.jsx
+++ src/components/Contact/SectionImage.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import { BlurhashCanvas } from "react-blurhash";
+import chantier from "../../assets/images/bg_hero_contact.webp"
+
 function SectionImage() {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const blurHash = "LfG+XRWXMxNG_NIpsSxZ.8Rkn$ay"; // lien BlurHash
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = chantier;
+    img.onload = () => setIsImageLoaded(true);
+  }, [chantier]);
+
   return (
-    <div className="px-4 md:px-32 py-11 h-80 lg:h-[80vh] ">
-      <div className="bg-image h-full w-full rounded-md"></div>
+    <div className="px-4 md:px-32 py-11 h-80 lg:h-[80vh]">
+      <div
+        className="h-full w-full rounded-md relative"
+        style={{
+          background: isImageLoaded
+            ? `url(${chantier}) center/cover no-repeat`
+            : "",
+        }}
+      >
+        {!isImageLoaded && (
+          <BlurhashCanvas
+            hash={blurHash}
+            className="absolute inset-0 w-full rounded-md h-full"
+          />
+        )}
+      </div>
     </div>
   );
 }
