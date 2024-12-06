4a8006e115d47649b3c1e4c51c70b7326f63c0e2
Hardy <hardsyldev@gmail.com>
2024-12-06 13:59:42 +0300

    add lazy loading to bg_hero

--- src/components/About/AboutHero.jsx
+++ src/components/About/AboutHero.jsx
@@ -1,9 +1,34 @@
 // import bg_hero from "../../assets/images/scene-construction-site-with-equipment.jpg"
+import { useEffect, useState } from "react";
 import "./About.css";
+import bg_hero from "../../assets/images/bg_apropos_hero.webp"
+import { BlurhashCanvas } from "react-blurhash";
 
 function AboutHero() {
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const blurHash = "LGIz-C~pD*^hD+slIpxZR*RkRkt6"; // lien BlurHash
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bg_hero;
+    img.onload = () => setIsImageLoaded(true);
+  }, [bg_hero]);
+
+
   return (
-    <section className="about-hero w-full mt-16 h-[30vh] flex justify-center items-center flex-col relative">
+    <section className="about-hero w-full mt-16 h-[30vh] flex justify-center items-center flex-col relative"
+      style={{
+        background: isImageLoaded
+          ? `url(${bg_hero}) center/cover no-repeat` : "",
+      }}>
+      {!isImageLoaded && (
+        <BlurhashCanvas
+          hash={blurHash}
+          className="absolute inset-0 w-full rounded-md h-full"
+        />
+      )}
       <div className="about-layer"></div>
       <h3 className="reveal text-green-500 text-xs">QUI SOMME-NOUS?</h3>
       <h1 className="reveal reveal-delay text-5xl text-white">A propos</h1>
