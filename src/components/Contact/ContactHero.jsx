7be220ee79bd6410efb544d0ab8e09ff69efbd7f
Hardy <hardsyldev@gmail.com>
2024-12-06 13:59:49 +0300

    add lazy loading to bg_hero

--- src/components/Contact/ContactHero.jsx
+++ src/components/Contact/ContactHero.jsx
@@ -1,8 +1,33 @@
 import "./Contact.css";
+import bg_hero from "../../assets/images/bg_hero_contact.webp"
+import { BlurhashCanvas } from "react-blurhash";
+import { useEffect, useState } from "react";
 
 function ContactHero() {
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const blurHash = "LgGu?zWXMxNG_NIps9xZ-=Rkn$ay"; // lien BlurHash
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bg_hero;
+    img.onload = () => setIsImageLoaded(true);
+  }, [bg_hero]);
+
+
   return (
-    <section className="contact-hero w-full mt-16 h-[30vh] flex justify-center items-center flex-col relative">
+    <section className="contact-hero w-full mt-16 h-[30vh] flex justify-center items-center flex-col relative"
+      style={{
+        background: isImageLoaded
+          ? `url(${bg_hero}) center/cover no-repeat` : "",
+      }}>
+      {!isImageLoaded && (
+        <BlurhashCanvas
+          hash={blurHash}
+          className="absolute inset-0 w-full rounded-md h-full"
+        />
+      )}
       <div className="contact-layer"></div>
       <h3 className="reveal text-green-500 text-xs">
         NOUS SOMME LA POUR VOUS!
