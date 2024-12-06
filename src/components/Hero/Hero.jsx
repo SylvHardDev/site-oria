712436c59f532b51fb2c12685737721993d875e2
Hardy <hardsyldev@gmail.com>
2024-12-06 13:30:35 +0300

    add lazy loading image bg hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,16 +6,41 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 // import Autoplay from "embla-carousel-autoplay";
-import { Button } from "../ui/button";        
+import { Button } from "../ui/button";
 import "./Hero.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import bg_hero from "../../assets/images/bg_home_hero.webp"
+import { BlurhashCanvas } from "react-blurhash";
 
 function Hero() {
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const blurHash = "L6G*l}0002xB010h?FWU0:xB0i}?"; // lien BlurHash
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bg_hero;
+    img.onload = () => setIsImageLoaded(true);
+  }, [bg_hero]);
+
+
   return (
     <div
       className="relative h-100% flex justify-center items-center mt-16"
       id="hero"
+      style={{
+        background: isImageLoaded
+          ? `url(${bg_hero}) center/cover no-repeat`: "",
+      }}
     >
+      {!isImageLoaded && (
+        <BlurhashCanvas
+          hash={blurHash}
+          className="absolute inset-0 w-full rounded-md h-full"
+        />
+      )}
       <div className="layer"></div>
       <Carousel
         className="w-2/3 md:h-60"
@@ -45,7 +70,7 @@ function Hero() {
           <CarouselItem className="flex flex-col items-center justify-around cursor-pointer">
             <h1 className="text-4xl lg:text-7xl tracking-wide">HCM</h1>
             <p className="md:w-2/3">
-            Terrassement, Assainissement, Construction Gros œuvre, Travaux de second Œuvre, Aménagement extérieur (Pavage, Espace vert,…). Agréer par SOCOTEC, APAVE, Suivi par LNTPB
+              Terrassement, Assainissement, Construction Gros œuvre, Travaux de second Œuvre, Aménagement extérieur (Pavage, Espace vert,…). Agréer par SOCOTEC, APAVE, Suivi par LNTPB
             </p>
             <div className="flex h-24 flex-col items-center justify-around md:flex-row md:justify-evenly md:w-[70%] lg:w-[50%] ">
               <Link to="/contact">
@@ -65,7 +90,7 @@ function Hero() {
             </p> */}
             <h1 className="text-4xl lg:text-7xl tracking-wide">SCI DM</h1>
             <p className="md:w-2/3">
-            Promotion immobilière : vente et/ou location terrain et immeuble (Appartement, Villa, bureaux, box commercial, dépôt)
+              Promotion immobilière : vente et/ou location terrain et immeuble (Appartement, Villa, bureaux, box commercial, dépôt)
             </p>
             <div className="flex h-24 flex-col items-center justify-around md:flex-row md:justify-evenly md:w-[70%] lg:w-[50%] ">
               <Link to="/contact">
